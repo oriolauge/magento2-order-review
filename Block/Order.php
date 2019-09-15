fa2d71a84bd3b233b6960a49e02596af2a4b5d23
Oriol Augé <oriolauge@gmail.com>
2019-09-15 12:32:30 +0200

    Add submit button and prepare form to send post to new controller

--- Block/Order.php
+++ Block/Order.php
@@ -3,4 +3,32 @@ namespace OAG\OrderReview\Block;
 class Order extends \Magento\Framework\View\Element\Template
 {
 
+    /**
+     * Holds module helper object class
+     * @var \OAG\OrderReview\Helper
+     */
+    protected $helper;
+
+    /**
+     * construct function
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \OAG\OrderReview\Helper\Data                     $helper
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \OAG\OrderReview\Helper\Data $helper
+    )
+    {        
+        $this->helper = $helper;
+        parent::__construct($context);
+    }
+
+    /**
+     * Get post url
+     * @return string
+     */
+    public function getAddReviewFormAction()
+    {
+        return $this->helper->getUrl('reviews/order/addreviewpost');
+    }
 }
